598a0e8ebd38e0f0584b5b4cc8fd92190c865e6e
Cyril Mukabwa <mukabwacyril@gmail.com>
2023-07-12 10:36:01 +0300

    first commit

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,19 +1,39 @@
+import { useState } from "react";
 import Button from "./Button";
 import Header from "./Header";
 import styles from './styles/PersonalInfo.module.css'
 
 export default function PersonalInfo(){
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [phone,setPhone] = useState('');
+  
+   const handleNameChange = e => setName(e.target.value);
+   const handleEmailChange = e => setEmail(e.target.value);
+   const handlePhoneChange = e => setPhone(e.target.value);
+
+   const handleSubmit = e =>{
+    e.preventDefault();
+    alert(`
+       Name: ${name}
+       Email: ${email}
+       Phne: ${phone};
+    `)
+   }
+  
     return (
         <div className={styles.container}>
             <Header title="Personal Info"
              details="Please provide your name,email address, and phone number."
             />
-            <form className={styles.form}>
+            <form onSubmit={handleSubmit} className={styles.form}>
               <div className={styles.inputDiv}>
 
                 <label htmlFor="name">Name</label>
                 <input type="text"
+                onChange={handleNameChange}
                 id="name"
+                
                 placeholder="e.g. Mucks Won"/>
               </div>
 
@@ -21,6 +41,7 @@ export default function PersonalInfo(){
               <label htmlFor="email">Email Address</label>
                 <input type="email"
                 id="email"
+                onChange={handleEmailChange}
                 placeholder="e.g. muckswon@gmail.com"/>
               </div>
 
@@ -28,10 +49,11 @@ export default function PersonalInfo(){
              <label htmlFor="name">Phone number</label>
                 <input type="text"
                 id="phone"
+                onChange={handlePhoneChange}
                 placeholder="e.g. +254 729 439 194"/>
              </div>
 
-                <Button  text="Next Step"/>
+                <Button  text="Next Step" type="next"/>
             </form>
         </div>
     )
